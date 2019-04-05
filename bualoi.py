63115c6c4dd7b10120749d019744dae70c409da8
harryboxer <zorrawit@gmail.com>
2019-04-05 13:51:58 +0700

    make pan

--- bualoi.py
+++ bualoi.py
@@ -1,5 +1,6 @@
 import arcade
 from random import randint
+from models import World
 from pyglet.window import key
 
 SCREEN_WIDTH = 980
@@ -11,57 +12,60 @@ xs = []
 ys = []
 n = 1
 
+class ModelSprite(arcade.Sprite):
+    def __init__(self, *args, **kwargs):
+        self.model = kwargs.pop('model', None)
 
-class Circle:
-    def __init__(self, x, y, vx, vy, r=20):
-        self.x = x
-        self.y = y
-        self.vx = vx
-        self.vy = vy
-        self.r = r
+        super().__init__(*args, **kwargs)
 
-    def move(self):
-        self.x += self.vx
-        self.y += self.vy
-
-        if self.x >= SCREEN_WIDTH - self.r:
-            self.vx -= 1
-        elif self.y >= SCREEN_HEIGHT - self.r:
-            self.vy -= 1
-        elif self.x <= self.r:
-            self.vx += 1
-        elif self.y <= self.r:
-            self.vy += 1
+    def sync_with_model(self):
+        if self.model:
+            self.set_position(self.model.x, self.model.y)
 
     def draw(self):
-        arcade.draw_circle_outline(self.x, self.y, 20, arcade.color.WHITE)
+        self.sync_with_model()
+        super().draw()
+
+# circle = Circle(700, 100, 0, 10, 20)
+# keys = key.KeyStateHandler()
 
 
-circle = Circle(700, 100, 0, 10, 20)
-keys = key.KeyStateHandler()
+# def move_circle(i):
+#     xs += vxs
+#     ys += vys
+
+class BualoiWindow(arcade.Window):
+    def __init__(self, width, height):
+        super().__init__(width, height)
+        arcade.set_background_color(arcade.color.WHITE_SMOKE)
+
+        self.world = World(SCREEN_WIDTH, SCREEN_HEIGHT)
+
+        self.pan = ModelSprite('images/handpan2.png', model = self.world.pan)
+
+    def on_draw(self):
+        arcade.start_render()
+        self.pan.draw()
+
+    def update(self, delta):
+        self.world.update(delta)
 
 
-def move_circle(i):
-    xs += vxs
-    ys += vys
+# def draw_circle(i):
+#     arcade.draw_circle_outline(xs, ys, 1, arcade.color.BLACK)
 
 
-def draw_circle(i):
-    arcade.draw_circle_outline(xs, ys, 1, arcade.color.BLACK)
-
-
-def on_draw(delta_time):
-    arcade.start_render()
-
-    circle.move()
-    circle.draw()
+    # circle.move()
+    # circle.draw()
 
 
 def main():
-    arcade.open_window(SCREEN_WIDTH, SCREEN_HEIGHT, "Circles")
-    arcade.set_background_color(arcade.color.GRAY)
-    arcade.get_window().push_handlers(keys)
-    arcade.schedule(on_draw, 1 / 80)
+    window = BualoiWindow(SCREEN_WIDTH, SCREEN_HEIGHT)
+    arcade.set_window(window)
+    # arcade.open_window(SCREEN_WIDTH, SCREEN_HEIGHT, "Circles")
+    # arcade.set_background_color(arcade.color.GRAY)
+    # arcade.get_window().push_handlers(keys)
+    # arcade.schedule(on_draw, 1 / 80)
     arcade.run()
 
 
--- models.py
+++ models.py
@@ -1,18 +1,49 @@
-class Hand:
+import arcade
+
+
+class Pan:
     def __init__(self, world, x, y):
         self.world = world
         self.x = x
         self.y = y
- 
+
     def update(self, delta):
         pass
- 
+
+
+class Circle:
+    def __init__(self, world, x, y, vx, vy, r=20):
+        self.world = world
+        self.x = x
+        self.y = y
+        self.vx = vx
+        self.vy = vy
+        self.r = r
+
+    def move(self):
+        self.x += self.vx
+        self.y += self.vy
+
+        if self.x >= self.world.width - self.r:
+            self.vx -= 1
+        elif self.y >= self.world.height - self.r:
+            self.vy -= 1
+        elif self.x <= self.r:
+            self.vx += 1
+        elif self.y <= self.r:
+            self.vy += 1
+
+    def draw(self):
+        arcade.draw_circle_outline(self.x, self.y, 20, arcade.color.WHITE)
+
+
 class World:
     def __init__(self, width, height):
         self.width = width
         self.height = height
- 
-        self.hand = Hand(self, width // 4, height // 3)
- 
+
+        self.pan = Pan(self, self.width // 4, self.height // 2)
+        self.circle = Circle(self, self.width // 4, self.height // 3, 0, 0)
+
     def update(self, delta):
-        self.hand.update(delta)
+        self.pan.update(delta)
