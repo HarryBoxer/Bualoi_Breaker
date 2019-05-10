3fb31639844283eb0d3baf7d28c24f361505355e
harryboxer <zorrawit@gmail.com>
2019-05-10 09:46:33 +0700

    clean code

--- bualoi.py
+++ bualoi.py
@@ -4,17 +4,12 @@ from random import randint
 from models import World
 from pyglet.window import key
 
+SCEEN_TITLE = "Bualoi"
+
 SCREEN_WIDTH = 980
 SCREEN_HEIGHT = 720
 
 CIRCLE_RADIUS = 20
-
-
-# vxs = []
-# vys = []
-# xs = []
-# ys = []
-# n = 1
 
 
 class ModelSprite(arcade.Sprite):
@@ -32,27 +27,6 @@ class ModelSprite(arcade.Sprite):
         self.sync_with_model()
         super().draw()
 
-# circle = Circle(700, 100, 0, 10, 20)
-# keys = key.KeyStateHandler()
-
-
-# def move_circle(i):
-#     xs += vxs
-#     ys += vys
-
-# p
-# class ball:
-#     def __init__(self, pan):
-#         self.ball = pan.ball
-#         self.angle = pan.angle
-#         self.x = pan.x
-#         self.y = pan.y
-
-#     def on_draw(self,angle):
-#         for ball in self.ball:
-#             arcade.draw_circle_filled(
-#                 self.x + (ball * math.cos(angle/55)), self.y + (ball * math.sin(angle/55))+20 , 10, arcade.color.YELLOW_ORANGE)
-
 
 class BualoiWindow(arcade.Window):
 
@@ -61,33 +35,20 @@ class BualoiWindow(arcade.Window):
         self.world = World(SCREEN_WIDTH, SCREEN_HEIGHT)
 
         arcade.set_background_color(arcade.color.WHITE_SMOKE)
-        # self.background = arcade.load_texture('images/background.png')
 
         self.pan = ModelSprite('images/handpan3.png', model=self.world.pan)
         self.circle = ModelSprite('images/cir.png', model=self.world.circle)
 
-        # self.bowl = ModelSprite('images/bowl.png', model=self.world.bowl)
-        # self.bowl = arcade.draw_circle_filled(self.world.circle.x, self.world.circle.y, 20, arcade.color.BLACK)
-
-        # self.ball = ball(self.world.pan)
-
     def on_draw(self):
-        # temp_radius = self.world.bowl.radius
         arcade.start_render()
-        # arcade.draw_texture_rectangle(SCREEN_WIDTH // 2, SCREEN_HEIGHT // 2,
-        #                                SCREEN_WIDTH, SCREEN_HEIGHT, self.background)
         self.pan.draw()
 
-        # if self.world.score >= 1:
-        #     temp_radius == 5 
         radius = self.world.bowl.radius
         color = self.world.bowl.color
-        arcade.draw_circle_filled(self.world.bowl.x, self.world.bowl.y,radius , color)
+        arcade.draw_circle_filled(
+            self.world.bowl.x, self.world.bowl.y, radius, color)
         self.circle.draw()
-        # self.bowl.draw()
         self.draw_score()
-        # p
-        # self.ball.on_draw(self.world.pan.angle)
 
     def draw_score(self):
         arcade.draw_text('Score : '+str(self.world.score),
@@ -99,14 +60,10 @@ class BualoiWindow(arcade.Window):
     def on_key_press(self, key, key_modifiers):
         if not self.world.is_started():
             self.world.start()
-            # self.world.reset()
         if key == arcade.key.SPACE:
             if self.world.count == 1:
                 self.world.on_key_press(key, key_modifiers)
             self.world.on_key_press(key, key_modifiers)
-
-    # def on_key_press(self, key, key_modifiers):
-    #     self.world.on_key_press(key, key_modifiers)
 
     def on_key_release(self, key, key_modifiers):
         self.world.on_key_release(key, key_modifiers)
@@ -115,19 +72,9 @@ class BualoiWindow(arcade.Window):
         self.world.update(delta)
 
 
-# def draw_circle(i):
-#     arcade.draw_circle_outline(xs, ys, 1, arcade.color.BLACK)
-    # circle.move()
-    # circle.draw()
-
-
 def main():
     window = BualoiWindow(SCREEN_WIDTH, SCREEN_HEIGHT)
     arcade.set_window(window)
-    # arcade.open_window(SCREEN_WIDTH, SCREEN_HEIGHT, "Circles")
-    # arcade.set_background_color(arcade.color.GRAY)
-    # arcade.get_window().push_handlers(keys)
-    # arcade.schedule(on_draw, 1 / 80)
     arcade.run()
 
 
